b8704ff4427b6e0203df1bee816cfc99e4e7179e
Daphne123-bit <153476899+Daphne123-bit@users.noreply.github.com>
2024-11-23 09:57:39 +0800

    Add files via upload

---
+++ bootstrap/text.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById("login").classList.add("active");
+    document.getElementById("signup").classList.remove("active");
+}
+
+function showSignup() {
+    document.getElementById("signup").classList.add("active");
+    document.getElementById("login").classList.remove("active");
+}
